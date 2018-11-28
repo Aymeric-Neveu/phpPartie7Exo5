81a5c68ad7748ae15a244b97de5f233237b86558
root <emploi.neveu@gmail.com>
2018-11-28 09:38:54 +0100

    exo5

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <select name="gender">
+      <option value="choose1">Mr</option>
+      <option value="choose2">Mme</option>
+    </select>
+    <form action="index.php" method="post">
+    <p>prénom<input type="text" name="prénom" value=""></p>
+    <p>nom<input type="text" name="nom" value=""></p>
+    <button type="submit" name="button">Valider</button>
+    </form>
+  </body>
+</html>
